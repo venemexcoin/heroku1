8b70b3d8018202bc21dcf54f16241cb4478cc097
venemexcoin <venemexcoin@gmail.com>
2021-08-07 18:34:47 -0500

    commit add logo & description eyelash

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- <meta html-equiv="refresh" content="0"> --}}
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/portafolio/developer-1.ico')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" />
     <title>@stack('title_base')</title>
     
